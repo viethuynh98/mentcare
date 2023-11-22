aae1713fbba98d5176b41933d4680b57c2e8d8c1
Viet Huynh <huynhviet98@gmail.com>
2023-11-22 23:59:17 +0700

    +init get drug name suggestion method

---
+++ app/getSuggestions.php
@@ -0,0 +1,18 @@
+<?php
+include('FormularyMedication.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $input = $_POST["input"];
+
+  // Tạo đối tượng FormularyMedication_DB_Test
+  $new_object = new \App\FormularyMedication_DB_Test('localhost', 'root', '', 'mentcare_db');
+
+  // Lấy danh sách gợi ý từ cơ sở dữ liệu
+  $suggestions = $new_object->getDrugSuggestions($input);
+
+  // Hiển thị danh sách gợi ý
+  foreach ($suggestions as $suggestion) {
+    echo "<div class='suggestion'>$suggestion</div>";
+  }
+}
+?>
