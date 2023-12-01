cdbfcd80f28cbb499077e6d8cbcb6590c9f4f107
Le Viet <viet.gm.2k3@gmail.com>
2023-12-01 22:12:31 +0700

    -update form design

--- app/showRecords.php
+++ app/showRecords.php
@@ -3,16 +3,18 @@ include('FormularyMedication.php');
 session_start();
 echo "<h1>Patient Records</h1>";
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $showPatientInfo = true; // Biến kiểm tra để xác định có hiển thị thông tin bệnh nhân hay không
+
     if (isset($_GET['patient_id'])) {
         $patient_id = $_GET['patient_id'];
 
         if ($patient_id) {
-
             $patient_records = $new_object->getPatientRecords($patient_id);
 
             if ($patient_records) {
-                echo '<div class = "full">';
-                echo '<div class = "record">';
+                // Hiển thị thông tin bệnh nhân nếu có tham số patient_id
+                echo '<div class="full_infor_of_patient">';
+                echo '<div class="record">';
                 echo "<h2>Patient Information</h2>";
                 echo "<h4>Medical History</h4>";
                 echo '<table>';
@@ -30,19 +32,22 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
                     echo '</tr>';
                 }
                 echo '</table>';
-                echo '<button><a href = "./form_02.php">New</a></button>';
+                echo '<button><a href="./form_02.php">New Prescription</a></button>';
                 echo '</div>';
             } else {
                 echo "No records found for the patient.";
             }
+
+            $showPatientInfo = false; // Không hiển thị phần else
         } else {
             echo "Patient ID is not provided.";
         }
     }
 
     if (isset($_GET['mh_id'])) {
+        // Hiển thị chi tiết đơn thuốc nếu có tham số mh_id
         $mh_id = $_GET['mh_id'];
-        echo '<div class = "detail">';
+        echo '<div class="detail">';
         echo "<h2>Prescription Records</h2>";
         $prescription_details = $new_object->getPrescriptionDetails($mh_id);
         foreach ($prescription_details as $prescription_detail) {
@@ -53,11 +58,42 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             echo "Note: " . $prescription_detail['note'] . "<br>";
             echo "<hr>";
         }
-        echo '<button><a href="./form_02.php?mh_id=' . $mh_id . '">New</a></button>';
+        echo '<button><a href="./form_02.php?mh_id=' . $mh_id . '">Reissue Prescription</a></button>';
         echo '</div>';
     }
+
+    // Hiển thị phần else khi cần
+    if ($showPatientInfo) {
+        $patient_records = $new_object->showPatientRecords();
+        echo '<div class="full_infor_of_patient">';
+        echo '<div class="record">';
+        echo "<h2>Patient Information</h2>";
+        echo "<h4>Medical History</h4>";
+        echo '<table>';
+        echo '<tr>';
+        echo '<th>Record</th>';
+        echo '<th>Details</th>';
+        echo '</tr>';
+        // Hiển thị thông tin bệnh nhân ở đây nếu không có tham số patient_id
+        foreach ($patient_records as $record) {
+            echo '<tr>';
+            echo "<td>Visit Date: " . $record['visit_date'] . "<br>" .
+                "Diagnose: " . $record['diagnose'] . "<br>" .
+                "Treatment: " . $record['treatment'] . "<br>" .
+                "</td>";
+            echo '<td><a href="?mh_id=' . $record["mh_id"] . '">Detail</a></td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+        echo '</div>';
+    }
+
     echo '</div>';
+} else {
+    // Hiển thị phần else khi không phải là GET request
+    // Code cho trường hợp không phải là GET request
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -66,123 +102,14 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/showRecords.css">
     <title>Document</title>
-    <style>
-        table,
-        th,
-        td {
-            border-collapse: collapse;
-            padding: 5px;
-            text-align: center;
-            margin: auto;
-        }
-
-        table {
-            border-radius: 10px;
-        }
-
-        tr:nth-child(even) {
-            background-color: #dfe6ea;
-        }
-
-        h1 {
-            text-align: center;
-            color: blue;
-        }
-
-        .full {
-            /* border: 2px solid red; */
-            display: flex;
-            /* justify-content: space-around; */
-            margin-top: 15vh;
-        }
-
-        .record {
-            width: 45vw;
-            /* border: 2px solid red; */
-
-        }
-
-        .record table {
-            width: 100%;
-            box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
-            font-size: 1em;
-            line-height: 1.4;
-            margin-bottom: 4vh;
-        }
-
-        .record h2,
-        h4 {
-            text-align: center;
-        }
-
-        .record a {
-            text-decoration: none;
-            color: black;
-            font-weight: 400;
-            width: 15vh;
-            padding: 1.5vh 0;
-
-        }
-
-        .record a:hover {
-            text-decoration: underline;
-            /* color: lightslategrey; */
-        }
-
-        .record button {
-            display: flex;
-            justify-content: center;
-            margin: 0 auto;
-            /* border: 1px solid black; */
-            border-radius: 10px;
-            background-color: lightblue;
-            font-size: 2.5vh;
-        }
-
-        .detail {
-            width: 45vw;
-            margin: auto;
-            /* float: right; */
-            /* border: 2px solid red; */
-        }
-
-        .detail h2 {
-            margin-bottom: 9vh;
-            text-align: center;
-        }
-
-        .detail button {
-            display: flex;
-            justify-content: center;
-            margin: 0 auto;
-            /* border: 1px solid black; */
-            border-radius: 10px;
-            background-color: lightblue;
-            font-size: 2.5vh;
-        }
-
-        .detail a {
-            text-decoration: none;
-            color: black;
-            font-weight: 400;
-            width: 15vh;
-            padding: 1.5vh 0;
-
-        }
-
-        .form {
-            position: absolute;
-            top: 12vh;
-            left: 5vh;
-        }
-    </style>
 </head>
 
 <body>
     <div class="form">
         <form action="" method="GET">
-            <input type="text" name="patient_id" placeholder="Enter ID_Patient" required>
+            <input type="text" name="patient_id" placeholder="  Enter ID_Patient" required>
             <button type="submit">Search</button>
         </form>
     </div>
