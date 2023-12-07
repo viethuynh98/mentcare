00e323c50e2d6ee88827229bc922565d0a2533d3
Le Viet <viet.gm.2k3@gmail.com>
2023-12-07 16:18:57 +0700

    -update header

--- app/showRecords.php
+++ app/showRecords.php
@@ -1,7 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="../css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/showRecords.css">
+    <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="../image/logo.jpg" rel="icon">
+    <title>Home Page</title>
+
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="d-flex align-items-center">
+        <div class="container d-flex justify-content-between">
+
+            <div id="logo">
+                <h1><a href="./interface.php">Mental Health<span>Care</span></a></h1>
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto" href="./interface.php">Home</a></li>
+                    <li><a class="nav-link scrollto active" href="./showRecords.php">Patient Record</a></li>
+                    <!-- <li><a class="nav-link scrollto" href="./form_02.php">Prescription</a></li> -->
+                    <li><a href="./log_out.php">Log Out</a></li>
+                </ul>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+    <!-- <h1>Patient Records</h1> -->
+    <div class="form">
+        <form action="" method="GET">
+            <input type="text" name="patient_id" placeholder="  Enter ID_Patient" required>
+            <button type="submit">Search</button>
+        </form>
+    </div>
+</body>
+
+</html>
 <?php
 include('FormularyMedication.php');
 session_start();
-echo "<h1>Patient Records</h1>";
+// echo "<h1>Patient Records</h1>";
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $showPatientInfo = true; // Biến kiểm tra để xác định có hiển thị thông tin bệnh nhân hay không
     $doctor_id = "DT01";
@@ -96,24 +143,3 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/showRecords.css">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="form">
-        <form action="" method="GET">
-            <input type="text" name="patient_id" placeholder="  Enter ID_Patient" required>
-            <button type="submit">Search</button>
-        </form>
-    </div>
-</body>
-
-</html>
